f47c22fb32f4bf8a6a2a7d107a8b8f32cb503e95
Alister Johnson <ajohnson@cs.uoregon.edu>
2018-05-25 14:21:03 -0700

    new version of script

--- tvbconn.py
+++ tvbconn.py
@@ -1,15 +1,19 @@
-from tvb.simulator.lab import *
 from tvb.simulator.plot.tools import *
 from mpl_toolkits.mplot3d import Axes3D
+from tvb.datatypes.connectivity import Connectivity
+from tvb.datatypes.cortex import Cortex
 from tvb.datatypes.region_mapping import RegionMapping
 from tvb.datatypes.sensors import SensorsEEG
 from tvb.datatypes.projections import ProjectionMatrix, ProjectionSurfaceEEG
-from tvb.simulator.models import WilsonCowan, Generic2dOscillator, ReducedWongWang, JansenRit
+from tvb.simulator import monitors
+from tvb.simulator.models import WilsonCowan, Generic2dOscillator, ReducedWongWang, JansenRit, Linear
+from tvb.simulator.integrators import VODE, VODEStochastic
+from tvb.simulator import simulator
 import time
 
 # zipped directory that contains connectivity/tractography info
 conn_fname = "/home/annajo/git/tvb/tvb-data/tvb_data/berlinSubjects/DH_20120806/connectivity/DH_20120806_Connectivity/DHconn.zip"
-conn = connectivity.Connectivity.from_file(conn_fname)
+conn = Connectivity(load_file=conn_fname)
 
 conn.configure()
 print("connectivity loaded")
@@ -22,11 +26,11 @@ plot_connectivity(connectivity=conn)
 ctx_fname = "/home/annajo/git/tvb/tvb-data/tvb_data/berlinSubjects/DH_20120806/cortex/DH_20120806_Surface_Cortex.zip"
 # maps nodes from cortex mesh to brain regions
 region_fname = "/home/annajo/git/tvb/tvb-data/tvb_data/berlinSubjects/DH_20120806/DH_20120806_RegionMapping.txt"
+rm = RegionMapping(load_file=region_fname)
 # matlab matrix that describes how waves propagate to the surface
 eeg_fname = "/home/annajo/git/tvb/tvb-data/tvb_data/berlinSubjects/DH_20120806/DH_20120806_ProjectionMatrix.mat"
-ctx = cortex.Cortex.from_file(source_file=ctx_fname,
-    region_mapping_file=region_fname,
-    eeg_projection_file=eeg_fname)
+ctx = Cortex(load_file=ctx_fname,
+    region_mapping_data=rm)
 ctx.configure()
 print("cortex loaded")
 
@@ -40,9 +44,8 @@ print("cortex plot ready")
 # unit vectors that describe the location of eeg sensors
 sensoreeg_fname = "/home/annajo/git/tvb/tvb-data/tvb_data/berlinSubjects/DH_20120806/DH_20120806_EEGLocations.txt"
 
-rm = RegionMapping.from_file(region_fname)
-sensorsEEG = SensorsEEG.from_file(sensoreeg_fname)
-prEEG = ProjectionSurfaceEEG.from_file(eeg_fname)
+sensorsEEG = SensorsEEG(load_file=sensoreeg_fname)
+prEEG = ProjectionSurfaceEEG(load_file=eeg_fname)
 
 fsamp = 1e3/1024.0 # 1024 Hz
 mon = monitors.EEG(sensors=sensorsEEG, projection=prEEG,
@@ -57,7 +60,7 @@ sim = simulator.Simulator(
     # model: Generic 2D Oscillator - neural mass has two state variables that
     # represent a neuron's membrane potential and recovery; see the
     # mathematics paper for default values and equations; runtime 8016 s
-    model=JansenRit(),
+    model=Linear(),
     # model: Wilson & Cowan - two neural masses have an excitatory/inhibitory
     # relationship; see paper for details; runtime 16031 s
     # model: Wong & Wang - reduced system of two non-linear coupled differential
@@ -67,11 +70,12 @@ sim = simulator.Simulator(
     # neuronal assemblies, with a particular focus on alpha activity; had
     # some weird numpy overflow errors, only generated ~200 ms data; runtime 5929 s
 
-    # integrator: Heun Deterministic
+
+    # integrator=VODEStochastic(),
     # initial conditions: None
     monitors=mon,
     surface=ctx,
-    simulation_length=1000.0 # ms
+    simulation_length=5.0 # ms
 ).configure()
 print("sim configured")
 
@@ -80,6 +84,8 @@ start = time.time()
 result = sim.run()[0]
 end = time.time()
 print("sim done, took " + str(end-start) + " seconds")
+
+print(result)
 pyplot.figure()
 time, data = result
 pyplot.plot(time, data[:, 0, :, 0], 'k', alpha=0.1)
